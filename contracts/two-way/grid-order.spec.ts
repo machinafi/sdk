ee3e316ace99e18284d47114ee8090350021dc2a
arobsn <87387688+arobsn@users.noreply.github.com>
2025-05-29 22:24:19 -0300

    fix script extension

--- contracts/two-way/grid-order.spec.ts
+++ contracts/two-way/grid-order.spec.ts
@@ -1,6 +1,6 @@
 import { describe } from "bun:test";
 
-const contract = await Bun.file("./contracts/two-way/grid-order.esc").text();
+const contract = await Bun.file("./contracts/two-way/grid-order.es").text();
 
 describe("contract loading", () => {
   console.log("contract", contract);

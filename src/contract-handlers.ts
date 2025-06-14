ae343a51e061ed3b8b4019ab4f0cef64f7f7ac33
arobsn <87387688+arobsn@users.noreply.github.com>
2025-06-14 12:08:14 -0300

    improve comments

--- src/contract-handlers.ts
+++ src/contract-handlers.ts
@@ -1,16 +1,16 @@
 import { ErgoTree } from "@fleet-sdk/core";
 import { hex } from "@fleet-sdk/crypto";
 
+// placeholder identifiers for base and quote tokens in contracts
 const TOKEN_ID_PLACEHOLDERS = {
-  base: "ba5e7acc110ee6374fe8fa7cd1e9ea4847e44dae4876d865cdffa61b4bdee03b", // Placeholder for the base token
-  quote: "cafe05e06b54b00eb0067c7c5e900c4d394030f4ac2e351f873a28f6158ced6e" // Placeholder for the quote token
+  base: "ba5e7acc110ee6374fe8fa7cd1e9ea4847e44dae4876d865cdffa61b4bdee03b",
+  quote: "cafe05e06b54b00eb0067c7c5e900c4d394030f4ac2e351f873a28f6158ced6e"
 };
 
 type BasePlaceholderId = typeof TOKEN_ID_PLACEHOLDERS.base;
 type QuotePlaceholderId = typeof TOKEN_ID_PLACEHOLDERS.quote;
 
 export type PlaceHolderId = BasePlaceholderId | QuotePlaceholderId;
-
 export interface PlaceHolder<T extends PlaceHolderId> {
   id: T;
   index: number;
